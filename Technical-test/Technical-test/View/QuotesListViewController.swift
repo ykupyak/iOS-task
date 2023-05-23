4a40a89ec3eb58857b2c574637217264501955d7
Yaroslav Kupyak <kupjak@gmail.com>
2023-05-23 18:37:07 +0300

    Implement API service request and parsing

--- Technical-test/Technical-test/View/QuotesListViewController.swift
+++ Technical-test/Technical-test/View/QuotesListViewController.swift
@@ -11,5 +11,23 @@ class QuotesListViewController: UIViewController {
     
     private let dataManager:DataManager = DataManager()
     private var market:Market? = nil
-    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Task {
+            do {
+                market = Market()
+                let quotes = try await dataManager.fetchQuotes()
+                market?.quotes = quotes.map { quote -> Quote in
+                    var updatedQuote = quote
+                    updatedQuote.myMarket = market
+                    return updatedQuote
+                }
+                print(market?.quotes ?? "Market not found")
+
+            } catch {
+                print("Error fetching quotes: \(error)")
+            }
+        }
+    }
 }
